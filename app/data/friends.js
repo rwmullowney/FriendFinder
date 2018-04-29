87330dfa544e2f515e0abcf8404ba6c904ea4d30
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-04-29 18:31:47 -0500

    Adds 10 questions to page and moves user list to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var users = [
+    {
+        name: "Bob",
+        photo: "website url",
+        responses: [5, 1, 4, 4, 3, 5, 2, 2, 4, 1]
+    },
+    {
+        name: "Steve",
+        photo: "Steve's website",
+        responses: [3, 5, 1, 1, 2, 5, 2, 4, 3, 3]
+    }
+];
+console.log(users);
