ebb4dfb005930d87f4fb3b0c9262e42b3f5e58b6
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-04-29 20:00:07 -0500

    Send user data to friends.js to be added to the users array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,14 @@
 var friendsList = require("../data/friends");
 
 module.exports = function(app){
+   
     app.get("/api/friends", function(req, res){
         res.json(friendsList)
     })
+
+    app.post("/api/friends", function(req, res){
+        // Sends the user info to the user list in friends.js
+        friendsList.push(req.body);
+    });
+
 };
